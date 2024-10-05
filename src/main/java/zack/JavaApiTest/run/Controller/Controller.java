aaf69b5254b3192679309040032928f58cfe870b
Zack Moore <19zackm96@gmail.com>
2024-10-05 09:36:30 -0400

    minor changes

--- src/main/java/zack/JavaApiTest/run/Controller/Controller.java
+++ src/main/java/zack/JavaApiTest/run/Controller/Controller.java
@@ -1,18 +1,54 @@
 package zack.JavaApiTest.run.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import zack.JavaApiTest.run.Run2;
+import zack.JavaApiTest.run.RunRepository;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/runs")
 public class Controller {
 
+    private final RunRepository runRepository;
+
+    public Controller(RunRepository runRepository) {
+        this.runRepository = runRepository;
+    }
+
+    // endpoint mapping scrambling? so no url injection can happen
     @GetMapping("/hello")
     String home() {
         return "Hello Runners!!";
     }
+
+    @GetMapping("")
+    List<Run2> runs() {
+        return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run2 findById(@PathVariable Integer id) {
+        Optional<Run2> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return run.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void postRun(@RequestBody Run2 run) {
+        runRepository.postRun(run);
+    }
+
+//    @GetMapping("/date")
+//    Run2 findByDate(@PathVariable LocalDateTime dateTime) {
+//        return runRepository.findByDate(dateTime);
+//    }
+
 }
--- src/main/java/zack/JavaApiTest/run/RunRepository.java
+++ src/main/java/zack/JavaApiTest/run/RunRepository.java
@@ -2,15 +2,35 @@ package zack.JavaApiTest.run;
 
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class RunRepository {
+
     private List<Run2> runs = new ArrayList<>();
 
-    List<Run2> findAll() {
+    public Optional<Run2> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
+    public void postRun(Run2 run) {
+        runs.add(run);
+    }
+
+//    public Run2 findByDate(LocalDateTime dateTime) {
+//        return runs.stream()
+//                filter(run -> run.date())
+//    }
+
+    public List<Run2> findAll() {
         return runs;
     }
 
@@ -19,7 +39,6 @@ public class RunRepository {
         runs.add(new Run2(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.OUTDOORS));
 
         runs.add(new Run2(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plusHours(2), 10, Location.INDOOR));
-
     }
 
 }
